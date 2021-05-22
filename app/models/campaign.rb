f9377258ef29083910de886b5b5cf5468af23285
Fernanda de Jesus <fernandabussular@gmail.com>
2021-05-22 16:43:05 -0300

    Add validates in models

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,10 @@
 class Campaign < ApplicationRecord
   belongs_to :instituition
+  has_many :donations
+  has_many :accountabilities
+
+  validates :name, presence :true
+  validates :description, presence :true
+  validates :category, presence :true
+  validates :total, presence :true
 end
--- app/models/instituition.rb
+++ app/models/instituition.rb
@@ -1,4 +1,8 @@
 class Instituition < ApplicationRecord
   belongs_to :user
   has_many :campaigns
+  validates :name, presence :true
+  validates :description, presence :true
+  validates :cnpj, presence :true
+  validates :address, presence :true
 end
