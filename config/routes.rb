2e099ef1d3920b27bbc4b815ee42f3fa692fd388
Raquel Lima <raqueljones182@gmail.com>
2021-06-03 20:33:19 -0300

    donation

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
+  
   resources :institutions do
-    resources :campaigns do
-      resources :accontability, only: [:create, :new, :show] do
-        resources :reviews, only: [:new, :create]
-      end
-    end
+    resources :campaigns, only: [:new, :create]
   end
-  resources :donations, only: [:new, :create]
-  resources :campaigns
+
+  resources :campaigns, only: [:show, :index, :edit, :update, :destroy] do
+    resources :donations, only: [:new, :create]
+    resources :accontability, only: [:create, :new]
+  end
+
+  resources :accontability, only: [:show] do
+    resources :reviews, only: [:new, :create]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
