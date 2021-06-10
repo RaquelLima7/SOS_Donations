661ba8e54b26c75b3e1b45db0f8bdfa660535c94
Alexandre Gambati <gambati@hotmail.com>
2021-06-10 07:19:52 -0300

    anonymous in views and etc

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -6,6 +6,20 @@ class CampaignsController < ApplicationController
     @campaigns = @campaigns.where(institution_id: params[:institution_id]) if params[:institution_id]
     @campaigns = @campaigns.where(category: params[:category]) if params[:category]
     @campaigns = @campaigns.where(type_donation: params[:type_donation]) if params[:type_donation]
+
+    @campaigns_title = "All Campaigns in SOS Donations!" if !params[:institution_id] && !params[:category] && !params[:type_donation]
+    @campaigns_title = "Campaigns from #{Institution.find(params[:institution_id]).name}" if params[:institution_id]
+    if @campaigns_title
+      @campaigns_title = "#{@campaigns_title} ,in #{params[:category]}" if params[:category]
+    else
+      @campaigns_title = "Campaigns in #{params[:category]}" if params[:category]
+    end
+    if @campaigns_title
+      @campaigns_title = "#{@campaigns_title} ,in #{params[:type_donation]}" if params[:type_donation]
+    else
+      @campaigns_title = "Campaigns in #{params[:type_donation]}" if params[:type_donation]
+    end
+
   end
 
   def show; end
