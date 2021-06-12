a491c421ac85b830e747247f1c87b5c50e402970
Raquel Lima <raqueljones182@gmail.com>
2021-06-12 17:41:29 -0300

    fix bug

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,5 +1,5 @@
 class CampaignsController < ApplicationController
-  before_action :set_campaign, only: [:show, :edit, :update]
+  before_action :set_campaign, only: %i[show edit update]
 
   def index
     @campaigns = Campaign.all
@@ -7,19 +7,20 @@ class CampaignsController < ApplicationController
     @campaigns = @campaigns.where(category: params[:category]) if params[:category]
     @campaigns = @campaigns.where(type_donation: params[:type_donation]) if params[:type_donation]
 
-    @campaigns_title = "All Campaigns in SOS Donations!" if !params[:institution_id] && !params[:category] && !params[:type_donation]
+    if !params[:institution_id] && !params[:category] && !params[:type_donation]
+      @campaigns_title = "All Campaigns in SOS Donations!"
+    end
     @campaigns_title = "Campaigns from #{Institution.find(params[:institution_id]).name}" if params[:institution_id]
     if @campaigns_title
       @campaigns_title = "#{@campaigns_title} ,in #{params[:category]}" if params[:category]
-    else
-      @campaigns_title = "Campaigns in #{params[:category]}" if params[:category]
+    elsif params[:category]
+      @campaigns_title = "Campaigns in #{params[:category]}"
     end
     if @campaigns_title
       @campaigns_title = "#{@campaigns_title} ,in #{params[:type_donation]}" if params[:type_donation]
-    else
-      @campaigns_title = "Campaigns in #{params[:type_donation]}" if params[:type_donation]
+    elsif params[:type_donation]
+      @campaigns_title = "Campaigns in #{params[:type_donation]}"
     end
-
   end
 
   def show; end
@@ -29,24 +30,24 @@ class CampaignsController < ApplicationController
     @campaign = Campaign.new
     authorize @campaign
   end
-  
 
   def create
     @institution = Institution.find(params[:institution_id])
     @campaign = Campaign.new(campaign_params)
     @campaign.institution = @institution
     if @campaign.photos.empty?
-      @campaign.photos.attach(io: File.open('app/assets/images/neutral-institution.jpg'), filename: 'neutral-institution.jpg', content_type: 'image/png')
+      @campaign.photos.attach(io: File.open('app/assets/images/neutral-institution.jpg'),
+                              filename: 'neutral-institution.jpg', content_type: 'image/png')
     end
     @campaign.raised = 0
     authorize @campaign
     if @campaign.save
       redirect_to campaign_path(@campaign)
     else
-      render :new 
+      render :new
     end
   end
-  
+
   def edit; end
 
   def update
@@ -55,6 +56,7 @@ class CampaignsController < ApplicationController
   end
 
   private
+
   def set_campaign
     @campaign = Campaign.find(params[:id])
     if @campaign.type_donation == "Volunteer"
